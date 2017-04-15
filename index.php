ead27730db78a002c318ccfc26f63a484f30a6a3
root <root@s18502272.onlinehome-server.com>
2017-04-15 13:29:08 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+    <Gather numDigits="5" timeout="10000" action="zip-lookup.php" method="GET">
+	<Say>
+		hello, please enter your 5 digit zip code
+	</Say>
+    </Gather>
+</Response>
