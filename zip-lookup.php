d62c51e7f8634a0bdf0aa26337aaf17e03992ccb
Danny Gershman <danny.gershman@gmail.com>
2017-04-16 14:35:30 -0400

    getting the city, state

--- zip-lookup.php
+++ zip-lookup.php
@@ -1,5 +1,15 @@
 <?php
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-    echo "<Response><Say>You entered " . $_REQUEST['Digits'] . "</Say></Response>";
-?>
+   
+    $digits = $_REQUEST['Digits'];
+	
+    if (strlen($digits) > 0) {
+        $map_details_response = file_get_contents("http://maps.googleapis.com/maps/api/geocode/json?address=" . $digits);
+        $map_details = json_decode($map_details_response);
+        $location = $map_details->results[0]->formatted_address;
+    }
+
+    echo "<Response><Say>Searching in " . $location . "</Say></Response>";
+    
+
