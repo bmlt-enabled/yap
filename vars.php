f87284013ca26e4731dcd5f8c41eafb102ca6418
Danny Gershman <danny.gershman@gmail.com>
2017-04-19 00:37:02 -0400

    changing to the global server

--- vars.php
+++ vars.php
@@ -6,7 +6,7 @@
     $google_maps_endpoint = "http://maps.googleapis.com/maps/api/geocode/json?address=";
     
     // TODO: Potentially allow different BMLT roots
-    $bmlt_root_server = "http://bmlt-aggregator.archsearch.org/c81e728d9d4c2f636f067f89cc14862c/bmltfed/main_server";
+    $bmlt_root_server = "http://bmlt-aggregator.archsearch.org/eccbc87e4b5ce2fe28308fd9f2a7baf3/bmltfed/main_server";
     $bmlt_search_endpoint = $bmlt_root_server . "/client_interface/json/index.php?switcher=GetSearchResults&sort_key=distance_in_miles,start_time&long_val={LONGITUDE}&lat_val={LATITUDE}&geo_width=-10&weekdays[]=" . $day;
 
     $results_count = 3;
