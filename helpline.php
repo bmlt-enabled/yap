63f07a6317a0c369fe4b85cecd73903c66774b3e
Danny Gershman <dgershman@securityscorecard.io>
2017-09-28 18:43:27 -0400

    testing out helpline changes

---
+++ helpline.php
@@ -0,0 +1,11 @@
+<?php
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+    <Gather numDigits="5" timeout="10000" action="searchtype.php" method="GET">
+	<Say>
+            Welcome to North Carolina of Narcotics Anonymous Helpline
+	</Say>
+    </Gather>
+</Response>
--- searchtype.php
+++ searchtype.php
@@ -5,8 +5,12 @@
     $digits = $_REQUEST['Digits'];
 ?>
 <Response>
-    <Gather numDigits="1" timeout="10000" action="zip-lookup.php?Zip=<?php echo $digits ?>" method="GET">
-	<Say>Press 1 to search for meetings today near you.</Say>
-        <Say>Press 2 to search for meetings upcoming</Say>
-    </Gather>
+    <?php if ($digits == "00000") { ?>
+        <Redirect method="GET">helpline.php</Redirect>
+    <?php } else { ?>
+        <Gather numDigits="1" timeout="10000" action="zip-lookup.php?Zip=<?php echo $digits ?>" method="GET">
+            <Say>Press 1 to search for meetings today near you.</Say>
+            <Say>Press 2 to search for meetings upcoming</Say>
+        </Gather>
+    <?php } ?>
 </Response>
