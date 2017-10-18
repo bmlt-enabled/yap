e2f53825e596458d69962ef4d0ba724c6cb0c624
Danny Gershman <dgershman@securityscorecard.io>
2017-10-18 00:05:32 -0400

    providing a message prior to putting on MOH

--- helpline-dial.php
+++ helpline-dial.php
@@ -4,5 +4,6 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>   
 <Response>
+    <Play>Please wait while we connect your call...</Play>
     <Enqueue><?php echo $_REQUEST['queue'] ?></Enqueue>
 </Response>
