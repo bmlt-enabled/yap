1e4c4d88e5ae73bd136300da4ce0d42566bb7454
Danny Gershman <danny.gershman@gmail.com>
2018-01-20 00:18:48 -0500

    adding new config values

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
 static $title = "";
-static $bmlt_root_server = "http://na-bmlt.org/_/sandwich";
+static $bmlt_root_server = "";
 static $voice = "woman";
 static $language = "en";
 static $google_maps_api_key = "";
 static $province_lookup = false;
+static $helpline_search_radius = 30; #in miles
+static $helpline_search_unpublished = false;
