869cdbd5585bf9f26224303914f38c9a4424fcbb
Danny Gershman <danny.gershman@gmail.com>
2018-02-05 22:07:45 -0500

    linting and fix for #10

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 .PHONY: upgrade
 
+lint:
+	find . -name '*.php' -exec php -l {} \;
+
 upgrade:
 	mv config.php ../
 	git pull origin master
--- voice-input-result.php
+++ voice-input-result.php
@@ -1,5 +1,6 @@
 <?php
     include 'config.php';
+    include 'functions.php';
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
     
