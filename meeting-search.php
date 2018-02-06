96d2c3bb1d945302ec830a8f6eb1e7086223eaba
Danny Gershman <danny.gershman@gmail.com>
2018-02-05 22:51:53 -0500

    avoid sms xml doc warnings

--- meeting-search.php
+++ meeting-search.php
@@ -29,43 +29,52 @@
         }
     }
 
-    if (count($search_results) == 0) {
-        echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">No results found, you probably have an invalid entry.  Try again.</Say><Redirect method=\"GET\">input-method.php?Digits=2</Redirect>";
-    } elseif (count($filtered_list) == 0) {
-        echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">There are no other meetings for today.  Thank you for calling, goodbye.</Say>";
-    } else {
-?>
-    <Say voice="<?php echo $voice; ?>" language="<?php echo $language; ?>">Meeting information found, listing the top <?php echo $results_count ?> results.</Say>
-<?php
-        $results_counter = 0;
-        for ($i = 0; $i < count($filtered_list); $i++) {
-            $result_day = $filtered_list[$i]->weekday_tinyint;
-            $result_time = $filtered_list[$i]->start_time;
-            
-            $part_1 = str_replace("&", "&amp;", $filtered_list[$i]->meeting_name);
-            $part_2 = str_replace("&", "&amp;", $GLOBALS['days_of_the_week'][$result_day]
-                    . ' ' . (new DateTime($result_time))->format('g:i A'));
-            $part_3 = str_replace("&", "&amp;", $filtered_list[$i]->location_street
-                    . " in " . $filtered_list[$i]->location_municipality
-                    . ", " . $filtered_list[$i]->location_province);
-
-            if (!isset($_REQUEST["SmsSid"])) {
-                echo "<Pause length=\"1\"/>";
-                echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">Result number " . ($results_counter + 1) . "</Say>";
-                echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">" . $part_1 . "</Say>";
-                echo "<Pause length=\"1\"/>";
-                echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">Starts at " . $part_2 . "</Say>";
-                echo "<Pause length=\"1\"/>";
-                echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">" . $part_3 . "</Say>";
-            }
-            
-            $message = $part_1 . $text_space . $part_2 . $text_space . $part_3;
-            echo "<Sms>" . $message . "</Sms>";
-            
-            $results_counter++;
-            if ($results_counter == $results_count) break;
+    if (!isset($_REQUEST["SmsSid"])) {
+        if (count($search_results) == 0) {
+            echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">No results found, you probably have an invalid entry.  Try again.</Say><Redirect method=\"GET\">input-method.php?Digits=2</Redirect>";
+        } elseif (count($filtered_list) == 0) {
+            echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">There are no other meetings for today.  Thank you for calling, goodbye.</Say>";
+        } else {
+            echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">Meeting information found, listing the top " . $results_count . " results.</Say>";
         }
-        
+    } else {
+        if (count($search_results) == 0) {
+            echo "<Sms>No results found, you probably have an invalid entry.  Try again.</Sms>";
+        } elseif (count($filtered_list) == 0) {
+            echo "<Sms>There are no other meetings for today.</Sms>";
+        }
+    }
+
+    $results_counter = 0;
+    for ($i = 0; $i < count($filtered_list); $i++) {
+        $result_day = $filtered_list[$i]->weekday_tinyint;
+        $result_time = $filtered_list[$i]->start_time;
+
+        $part_1 = str_replace("&", "&amp;", $filtered_list[$i]->meeting_name);
+        $part_2 = str_replace("&", "&amp;", $GLOBALS['days_of_the_week'][$result_day]
+                . ' ' . (new DateTime($result_time))->format('g:i A'));
+        $part_3 = str_replace("&", "&amp;", $filtered_list[$i]->location_street
+                . " in " . $filtered_list[$i]->location_municipality
+                . ", " . $filtered_list[$i]->location_province);
+
+        if (!isset($_REQUEST["SmsSid"])) {
+            echo "<Pause length=\"1\"/>";
+            echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">Result number " . ($results_counter + 1) . "</Say>";
+            echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">" . $part_1 . "</Say>";
+            echo "<Pause length=\"1\"/>";
+            echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">Starts at " . $part_2 . "</Say>";
+            echo "<Pause length=\"1\"/>";
+            echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">" . $part_3 . "</Say>";
+        }
+
+        $message = $part_1 . $text_space . $part_2 . $text_space . $part_3;
+        echo "<Sms>" . $message . "</Sms>";
+            
+        $results_counter++;
+        if ($results_counter == $results_count) break;
+    }
+
+    if (!isset($_REQUEST["SmsSid"])) {
         echo "<Say voice=\"" . $voice . "\" language=\"" . $language . "\">Thank you for calling, goodbye.</Say>";
     }
 ?>
