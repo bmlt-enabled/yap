3a09a71c23f4fc625eb1f1cf9723f35282c70bcf
Danny Gershman <dgershman@securityscorecard.io>
2018-04-17 22:27:59 -0400

    relative paths

--- admin/api/authenticate.php
+++ admin/api/authenticate.php
@@ -3,7 +3,7 @@
     include '../../functions.php';
 
     if (auth_bmlt($_POST['username'], $_POST['password'])) {
-        header('Location: /admin/dashboard.php');
+        header('Location: ../dashboard.php');
     } else {
-        header('Location: /admin/index.php?auth=false');
+        header('Location: ../index.php?auth=false');
     }
--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,14 @@
-<?php include_once 'header.php'?>
-<link href="/admin/css/signin.css" rel="stylesheet">
+<?php
+    include_once 'header.php';
+
+    if ($_SERVER['REQUEST_URI'] == "/admin") {
+        header('Location: /admin/', null, 301);
+    };
+?>
+<link href="css/signin.css" rel="stylesheet">
 <div class="container">
 
-    <form class="form-signin" method="POST" action="/admin/api/authenticate.php">
+    <form class="form-signin" method="POST" action="api/authenticate.php">
         <h2 class="form-signin-heading">Yap</h2>
         <div id="no-auth-message"><?php echo isset($_REQUEST["auth"]) ? "Not Authorized" : "" ?></div>
         <label for="inputEmail" class="sr-only">Username</label>
@@ -13,4 +19,5 @@
     </form>
 
 </div> <!-- /container -->
-<?php include_once 'footer.php'?>
+<?php
+    include_once 'footer.php';
