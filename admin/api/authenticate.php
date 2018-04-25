d94be06020c14204b826270336f1439bb539a766
Danny Gershman <dgershman@securityscorecard.io>
2018-04-24 20:37:39 -0400

    removing admin portal from 1.x

--- admin/api/authenticate.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include '../../config.php';
-    include '../../functions.php';
-
-    if (auth_bmlt($_POST['username'], $_POST['password'])) {
-        header('Location: /admin/dashboard.php');
-    } else {
-        header('Location: /admin/index.php?auth=false');
-    }
