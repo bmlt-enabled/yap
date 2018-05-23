e6abb1c41293dc3c977da0ba78e0ac067a273d07
pjaudiomv <pjaudiomv@gmail.com>
2018-05-22 21:09:11 -0400

    change for one instance of pause

--- meeting-search.php
+++ meeting-search.php
@@ -69,8 +69,8 @@
 
     // Do not handle for the SMS gateway
     if (!isset($_REQUEST["SmsSid"]) && count($filtered_list) > 0) {
-        if (count($sms_messages) > 0) { 
-          echo "<Pause length=\"2\"/>"; ?>
+      echo "<Pause length=\"2\"/>";
+        if (count($sms_messages) > 0) {  ?>
             <Say voice="<?php echo $voice ?>" language="<?php echo $language ?>">
                 <?php echo word( 'press' ) ?> <?php echo word( "one" ) ?> <?php echo word( 'if_you_would_like_these_results_texted_to_you' ) ?>
             </Say>
@@ -83,8 +83,7 @@
             <Gather numDigits="1" timeout="10"
                     action="post-call-action.php?Payload=<?php echo urlencode( json_encode( $sms_messages ) ) ?>"
                     method="GET"/>
-        <?php } elseif (isset($GLOBALS['infinite_searching']) && $GLOBALS['infinite_searching']) { 
-          echo "<Pause length=\"2\"/>"; ?>
+        <?php } elseif (isset($GLOBALS['infinite_searching']) && $GLOBALS['infinite_searching']) {  ?>
             <Say voice="<?php echo $voice ?>" language="<?php echo $language ?>">
                 <?php echo word('press')?> <?php echo word("two")?> <?php echo word('if_you_would_like_to_search_again') ?>.
             </Say>
