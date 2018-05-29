aa08ed3f834283bc74d8115097d01d0889725aed
Danny Gershman <dgershman@securityscorecard.io>
2018-05-28 20:30:24 -0400

    double fire regression

--- admin/header.php
+++ admin/header.php
@@ -12,10 +12,6 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap-4.1.0.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <link rel="stylesheet" href="css/yap.css">
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/jquery-ui-1.12.1.min.js"></script>
-    <script src="js/bootstrap-4.1.0.min.js"></script>
-    <script src="js/yap.js"></script>
     <title>Yap Admin</title>
 </head>
 <body>
