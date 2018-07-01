f555dd98693458a211f2e07fcc13e441dda8e5cc
Danny Gershman <dgershman@securityscorecard.io>
2018-06-30 22:36:38 -0400

    hack

--- fbmessenger-gateway.php
+++ fbmessenger-gateway.php
@@ -9,4 +9,5 @@ if (isset($_REQUEST['hub_verify_token']) && $_REQUEST['hub_verify_token'] === $G
 $input = json_decode(file_get_contents('php://input'), true);
 error_log(json_encode($input));
 error_log("HTTP_HOST:".$_SERVER['HTTP_HOST']);
-async_post("https://".$_SERVER['HTTP_HOST']."/fbmessenger-process.php", $input);
+// TODO: This is hack!!!
+async_post("https://".$_SERVER['HTTP_HOST']."/yap/fbmessenger-process.php", $input);
