5ff1a9b4f1b85fd4c9dc815c70fa48783245c33a
Danny Gershman <dgershman@securityscorecard.io>
2018-07-06 20:06:18 -0400

    adding some upgrade advisor tools

--- upgrade-advisor.php
+++ upgrade-advisor.php
@@ -1,5 +1,6 @@
 <?php
     include 'config.php';
+    include 'functions.php';
 
 
     $all_good = true;
@@ -20,8 +21,20 @@
     foreach ($settings as $setting) {
         if (!isThere($setting)) {
             echo "Missing required setting: " . $setting . "<br/>";
-            $all_good = false;
+            exit();
         }
+    }
+
+    $root_server_settings = json_decode(get(getHelplineBMLTRootServer() . "/client_interface/json/?switcher=GetServerInfo"));
+
+    if (!isset($root_server_settings)) {
+        echo "Your root server returned no server information.  Double-check that you have the right root server url.";
+        exit();
+    }
+
+    if ($root_server_settings[0]->semanticAdmin != "1") {
+        echo "Semantic Admin not enabled on your root server, be sure to set the variable mentioned <a target=\"_blank\" href=\"https://bmlt.magshare.net/semantic/semantic-administration/\">here</a>.";
+        exit();
     }
 
     if ($all_good) {
