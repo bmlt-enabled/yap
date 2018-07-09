b6347d6dabf390f3be19afbdd557baba3a1fda3c
Danny Gershman <dgershman@securityscorecard.io>
2018-07-08 22:41:33 -0400

    bug with turning language selections off

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
-    if (has_setting('language_selections')) {
+    if (strlen(setting('language_selections')) > 0) {
         if (!isset($_REQUEST["Digits"])) {
             header( 'Location: /language-selector.php' );
             exit();
