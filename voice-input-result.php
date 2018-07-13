f09825aac78399d5fdb2edb3de7961d3cc66b7e7
Danny Gershman <dgershman@securityscorecard.io>
2018-07-13 01:46:39 -0400

    regression with province lookup

--- voice-input-result.php
+++ voice-input-result.php
@@ -3,7 +3,7 @@
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
     
-    $province = has_setting('province_lookup') ? $_REQUEST['Province'] : getProvince();
+    $province = has_setting('province_lookup') && setting('province_lookup') ? $_REQUEST['Province'] : getProvince();
     $speechResult = $_REQUEST['SpeechResult'];
     $searchType = $_REQUEST['SearchType'];
     
