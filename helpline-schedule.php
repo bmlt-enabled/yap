5edf891f7a2475308dd5a36d813da23a84cf31d6
Danny Gershman <dgershman@securityscorecard.io>
2018-07-22 12:56:13 -0400

    catching exception on schedule

--- helpline-schedule.php
+++ helpline-schedule.php
@@ -1,4 +1,8 @@
 <?php
 include 'functions.php';
 header("content-type: application/json");
-echo filterOut(getHelplineSchedule(setting("service_body_id")));
+try {
+    echo filterOut( getHelplineSchedule( setting( "service_body_id" ) ) );
+} catch (NoVolunteersException $nve) {
+    echo json_encode(new StdClass());
+}
