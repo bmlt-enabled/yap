1850261f901656538252d6e70ed8d5a704fdcab3
Danny Gershman <danny.gershman@gmail.com>
2018-08-14 22:33:56 -0400

    version bump

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'config.php';
 include_once 'session.php';
-static $version = "2.1.2";
+static $version = "2.1.3";
 static $settings_whitelist = [
     'bmlt_root_server' => [ 'description' => '' , 'default' => '', 'overridable' => false],
     'fallback_number' => [ 'description' => '' , 'default' => '', 'overridable' => true],
