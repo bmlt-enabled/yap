00a35e95a90a278d78330aa9699e1578e897d826
Danny Gershman <danny.gershman@gmail.com>
2018-10-18 21:35:01 -0400

    more logging

--- helpline-answer-response.php
+++ helpline-answer-response.php
@@ -4,13 +4,15 @@ include 'functions.php';
 header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";?>
 <Response>
-<?php if ($_REQUEST['Digits'] == "1") {?>
+<?php if ($_REQUEST['Digits'] == "1") {
+    log_debug("They took the call.") ?>
     <Dial>
         <Conference endConferenceOnExit="true" startConferenceOnEnter="true" beep="false">
             <?php echo $_REQUEST['conference_name'] ?>
         </Conference>
     </Dial>
-<?php } else { ?>
+<?php } else {
+    log_debug("They rejected the call.") ?>
     <Hangup/>
 <?php } ?>
 </Response>
