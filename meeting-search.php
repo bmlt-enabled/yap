01fd6b83519a11f6254ea35e953a3521190fe81b
rdtripp <44327749+rdtripp@users.noreply.github.com>
2018-11-03 17:24:22 -0500

    Update meeting-search.php

--- meeting-search.php
+++ meeting-search.php
@@ -5,14 +5,12 @@
     use Twilio\Rest\Client;
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-
     $sid = $GLOBALS['twilio_account_sid'];
     $token = $GLOBALS['twilio_auth_token'];
     $client = new Client( $sid, $token );
     
     $latitude = isset($_REQUEST['Latitude']) ? $_REQUEST['Latitude'] : null;
     $longitude = isset($_REQUEST['Longitude']) ? $_REQUEST['Longitude'] : null;
-
     try {
         $results_count = has_setting('result_count_max') ? setting('result_count_max') : 5;
         $meeting_results = getMeetings($latitude, $longitude, $results_count, null, null);
@@ -24,10 +22,8 @@
         <?php
         exit;
     }
-
     $filtered_list = $meeting_results->filteredList;
     $sms_messages = [];
-
     $text_space = " ";
     $message = "";
 ?>
@@ -48,22 +44,19 @@
             echo "<Sms>" . word('there_are_no_other_meetings_for_today') . "</Sms>";
         }
     }
-
     $results_counter = 0;
     for ($i = 0; $i < count($filtered_list); $i++) {
         $results = getResultsString($filtered_list[$i]);
-
         if (!isset($_REQUEST["SmsSid"])) {
             echo "<Pause length=\"1\"/>";
             echo "<Say voice=\"" . setting('voice') . "\" language=\"" . setting('language') . "\">" . word('number') . " " . ($results_counter + 1) . "</Say>";
-            echo "<Say voice=\"" . setting('voice') . "\" language=\"" . setting('language') . "\">" . $results[0] . "</Say>";
-            echo "<Pause length=\"1\"/>";
+	    echo "<Say voice=\"" . setting('voice') . "\" language=\"" . setting('language') . "\">" . $results[0] . "</Say>";
+	    echo "<Pause length=\"1\"/>";
             echo "<Say voice=\"" . setting('voice') . "\" language=\"" . setting('language') . "\">" . word('starts_at') . " " . $results[1] . "</Say>";
-            echo "<Pause length=\"1\"/>";
-            preg_replace($city_pron_fix_a, $city_pron_fix_b,
-            echo "<Say voice=\"" . setting('voice') . "\" language=\"" . setting('language') . "\">" . $results[2] . "</Say>";
+	    echo "<Pause length=\"1\"/>";
+	    $results[2] = preg_replace($city_pron_fix_a, $city_pron_fix_b, $results[2]);
+	    echo "<Say voice=\"" . setting('voice') . "\" language=\"" . setting('language') . "\">" . $results[2] . "</Say>";
         }
-
         if (json_decode(setting('include_map_link'))) $results[2] .= " https://google.com/maps?q=" . $filtered_list[$i]->latitude . "," . $filtered_list[$i]->longitude;
         $message = $results[0] . $text_space . $results[1] . $text_space . $results[2];
         log_debug($message);
@@ -78,7 +71,6 @@
         $results_counter++;
         if ($results_counter == $results_count) break;
     }
-
     // Do not handle for the SMS gateway
     if (!isset($_REQUEST["SmsSid"]) && count($filtered_list) > 0) {
         echo "<Pause length=\"2\"/>";
@@ -103,7 +95,7 @@
         <?php } ?>
 
     <Say voice="<?php echo setting('voice') ?>" language="<?php echo setting('language') ?>">
-        <?php echo word('meeting_search_more_info')?> 
+	<?php echo word('meeting_search_more_info')?>
         <?php echo word('thank_you_for_calling_goodbye')?>
     </Say>
     <?php } ?>
