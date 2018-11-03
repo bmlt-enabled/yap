6bc0a7d68bf3cccb80e1becd9a22118fe8067f27
rdtripp <44327749+rdtripp@users.noreply.github.com>
2018-11-03 07:44:12 -0500

    Update index.php
    Changes made to index.php to add voice content. 
    #Two additional lines and two new variables, pre_title_greeting and post_title_greeting
    this is controlled by config.php as per usual.

--- index.php
+++ index.php
@@ -31,7 +31,9 @@
         <?php } else { ?>
             <?php if ( ! isset( $_REQUEST["Digits"] ) ) { ?>
                 <Say voice="<?php echo setting( 'voice' ) ?>" language="<?php echo setting( 'language' ) ?>">
+                    <?php echo setting( 'pre_title_greeting' ) ?>  #added this line
                     <?php echo setting( 'title' ) ?>
+                    <?php echo setting( 'post_title_greeting' ) ?>
                 </Say>
             <?php } ?>
             <Say voice="<?php echo setting( 'voice' ) ?>" language="<?php echo setting( 'language' ) ?>">
