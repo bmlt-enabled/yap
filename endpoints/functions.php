46acde72f4da423b45027a3fc4e12f1fc66ae14a
Danny Gershman <danny.gershman@gmail.com>
2018-11-09 18:46:55 -0500

    updating version

--- endpoints/functions.php
+++ endpoints/functions.php
@@ -2,7 +2,7 @@
 include_once '../config.php';
 include_once 'logging.php';
 include_once 'session.php';
-static $version = "2.5.0";
+static $version = "3.0.0-alpha2";
 static $settings_whitelist = [
     'blocklist' => [ 'description' => '' , 'default' => '', 'overridable' => true],
     'bmlt_root_server' => [ 'description' => '' , 'default' => '', 'overridable' => false],
