63ba4aec22ad7fc0a4a3eb61f1534f2c8da678b7
Danny Gershman <danny.gershman@gmail.com>
2019-01-31 20:29:27 -0500

    remove encoding

--- meeting-search.php
+++ meeting-search.php
@@ -83,7 +83,7 @@
             $webhook_url = $voice_url;
         }
 
-        $message = "Meeting Results: " . $webhook_url . htmlentities("/meeting-results.php?latitude=" . $latitude . "&longitude=" . $longitude);
+        $message = "Meeting Results: " . $webhook_url . "/meeting-results.php?latitude=" . $latitude . "&longitude=" . $longitude;
         sendSms($message);
     } else {
         $results_counter = 0;
