93885645584bebcaead6c4fbd69cb8c47da49710
Danny Gershman <danny.gershman@gmail.com>
2019-01-31 21:08:01 -0500

    backporting #230

--- endpoints/meeting-search.php
+++ endpoints/meeting-search.php
@@ -25,6 +25,12 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 
     $text_space = " ";
     $message = "";
+
+    function sendSms($message) {
+        if (isset($_REQUEST['From']) && isset($_REQUEST['To'])) {
+            $GLOBALS['client']->messages->create($_REQUEST['From'], array("from" => $_REQUEST['To'], "body" => $message));
+        }
+    }
 ?>
 <Response>
 <?php
@@ -55,23 +61,40 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
             echo "<Pause length=\"1\"/>";
             echo "<Say voice=\"" . setting('voice') . "\" language=\"" . setting('language') . "\">" . word('starts_at') . " " . $results[1] . "</Say>";
             echo "<Pause length=\"1\"/>";
-            echo "<Say voice=\"" . setting('voice') . "\" language=\"" . setting('language') . "\">" . (has_setting('include_location_text') && json_decode(setting('include_location_text')) ? $results[2] . " " . $results[3] : $results[3]) . "</Say>";
         }
 
-        if (json_decode(setting('include_map_link'))) $results[3] .= " https://google.com/maps?q=" . $filtered_list[$i]->latitude . "," . $filtered_list[$i]->longitude;
-        if (has_setting('include_location_text') && json_decode(setting('include_location_text'))) $results[1] .= $text_space . $results[2];
-        $message = $results[0] . $text_space . $results[1] . $text_space . $results[3];
-        log_debug($message);
-        if (json_decode(setting("sms_ask")) && !isset($_REQUEST["SmsSid"])) {
-            array_push($sms_messages, $message);
-        } else {
-            if (isset($_REQUEST['From']) && isset($_REQUEST['To'])) {
-            	$twilioClient->messages->create($_REQUEST['From'], array("from" => $_REQUEST['To'], "body" => $message));
-            }
-        }
-            
         $results_counter++;
         if ($results_counter == $results_count) break;
+    }
+
+    if (has_setting('sms_summary_page') && json_decode(setting('sms_summary_page'))) {
+        $voice_url = "https://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+        if (strpos(basename($voice_url), ".php")) {
+            $webhook_url = substr( $voice_url, 0, strrpos( $voice_url, "/" ) );
+        } else if (strpos($voice_url, "?")) {
+            $webhook_url = substr( $voice_url, 0, strrpos( $voice_url, "?" ) );
+        } else {
+            $webhook_url = $voice_url;
+        }
+
+        $message = "Meeting Results: " . $webhook_url . "/meeting-results.php?latitude=" . $latitude . "&longitude=" . $longitude;
+        sendSms($message);
+    } else {
+        $results_counter = 0;
+        for ($i = 0; $i < count($filtered_list); $i++) {
+            $results = getResultsString($filtered_list[$i]);
+            if (has_setting('include_location_text') && json_decode(setting('include_location_text'))) $results[1] .= $text_space . $results[2];
+            $message = $results[0] . $text_space . $results[1] . $text_space . $results[3];
+
+            if (json_decode(setting("sms_ask")) && !isset($_REQUEST["SmsSid"])) {
+                array_push($sms_messages, $message);
+            } else {
+                sendSms($message);
+            }
+
+            $results_counter++;
+            if ($results_counter == $results_count) break;
+        }
     }
 
     // Do not handle for the SMS gateway
