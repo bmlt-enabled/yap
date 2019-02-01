5cabd4c707810cb09e4a5c4ccb8c4d23ac27c65a
Danny Gershman <danny.gershman@gmail.com>
2019-01-31 22:08:15 -0500

    responsive backport

--- endpoints/meeting-results.php
+++ endpoints/meeting-results.php
@@ -1,11 +1,11 @@
 <?php include_once 'functions.php';?>
 <html>
 <body>
-<table border="1">
+<table border="1" width="100%">
     <?php
     $meeting_results = getMeetings($_REQUEST['latitude'], $_REQUEST['longitude'], 50, null, null);
     foreach ($meeting_results->filteredList as $meeting) { ?>
-        <tr><td>
+        <tr><td style="font-size: 24px;">
                 <?php
                 $results = getResultsString($meeting);
                 foreach ($results as $result) { ?>
