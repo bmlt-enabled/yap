f917b14c8411323f79c18f218911d155a73f83f5
Danny Gershman <danny.gershman@gmail.com>
2019-02-02 19:20:13 -0500

    deactive the session check feature for now

--- admin/nav.php
+++ admin/nav.php
@@ -3,13 +3,14 @@ require_once 'auth_verify.php';
 require_once 'header.php';
 ?>
 <script type="text/javascript">
-    setInterval(function() {
+    // TODO: This is actually keeping the session alive.
+    /*setInterval(function() {
         $.get("auth_verify.php?format=json", function(data) {
             if (data["expired"]) {
                 location.href='index.php?expired=true';
             }
         })
-    }, 60000);
+    }, 60000);*/
 </script>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="#">Yap</a>
