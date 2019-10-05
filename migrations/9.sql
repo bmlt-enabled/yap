c5b2c3580eb1d44b188835180f87a2a581a74069
Danny Gershman <danny.gershman@gmail.com>
2019-10-05 10:50:51 -0400

    adding records_events table

--- migrations/9.sql
+++ migrations/9.sql
@@ -1,11 +1,8 @@
-CREATE TABLE `records` (
+CREATE TABLE `records_events` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `callsid` varchar(255) DEFAULT NULL,
   `start_time` varchar(45) DEFAULT NULL,
   `end_time` varchar(45) DEFAULT NULL,
-  `from` varchar(255) NOT NULL,
-  `to` varchar(255) DEFAULT NULL,
-  `payload` longtext,
-  `duration` int(11) DEFAULT NULL,
+  `event_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
