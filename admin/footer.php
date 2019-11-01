1865d8384890d9bdfee2c68f4b096740f9dca330
Danny Gershman <danny.gershman@gmail.com>
2019-11-01 00:00:01 -0400

    adding script debugging flag

--- admin/footer.php
+++ admin/footer.php
@@ -3,6 +3,6 @@
 <script src="vendor/popper.js/dist/umd/popper.min.js"></script>
 <script src="vendor/bootstrap/dist/js/bootstrap.bundle.js"></script>
 <script src="vendor/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
-<script src="js/yap.js?v=<?php echo time()?>"></script>
+<script src="js/yap.js<?php isset($_REQUEST['JSDEBUG']) ? sprintf("?v=%s", time()) : "";?>"></script>
 </body>
 </html>
