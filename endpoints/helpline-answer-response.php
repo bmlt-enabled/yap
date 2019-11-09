e5a8440197fd8c531b5046ed216dbf4049ad7b43
Danny <danny.gershman@gmail.com>
2019-11-08 22:45:13 -0800

    better call blasting pt 2

--- endpoints/helpline-answer-response.php
+++ endpoints/helpline-answer-response.php
@@ -1,22 +1,34 @@
 <?php
 require_once '_includes/functions.php';
+require_once '_includes/twilio-client.php';
 header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";?>
 <Response>
 <?php if ($_REQUEST['Digits'] == "1") {
     log_debug("They took the call.");
-    insertCallEventRecord(EventId::VOLUNTEER_IN_CONFERENCE, (object)["to_number" => $_REQUEST['Called']]);
-    ?>
-    <Dial>
-        <Conference
-            statusCallbackMethod="GET"
-            statusCallbackEvent="join"
-            endConferenceOnExit="true"
-            startConferenceOnEnter="true"
-            beep="false">
-            <?php echo $_REQUEST['conference_name'] ?>
-        </Conference>
-    </Dial>
+    $conferences = $twilioClient->conferences->read(array ("friendlyName" => $_REQUEST['conference_name'] ));
+    $participants = $twilioClient->conferences($conferences[0]->sid)->participants->read();
+
+    if (count($participants) == 2) {
+        error_log("Enough volunteers have joined.  Hanging up this volunteer.") ?>
+        <Say voice="<?php echo voice(); ?>" language="<?php echo setting('language') ?>">
+            <?php echo word('volunteer_has_already_joined_the_call_goodbye'); ?>
+        </Say>
+        <Hangup/>
+    <?php
+    } else {
+        insertCallEventRecord(EventId::VOLUNTEER_IN_CONFERENCE, (object)["to_number" => $_REQUEST['Called']]); ?>
+        <Dial>
+            <Conference
+                statusCallbackMethod="GET"
+                statusCallbackEvent="join"
+                endConferenceOnExit="true"
+                startConferenceOnEnter="true"
+                beep="false">
+                <?php echo $_REQUEST['conference_name'] ?>
+            </Conference>
+        </Dial>
+    <?php } ?>
 <?php } else {
     insertCallEventRecord(EventId::VOLUNTEER_REJECTED, (object)["digits" => $_REQUEST['Digits']]);
     setConferenceParticipant($_REQUEST['conference_name'], CallRole::VOLUNTEER);
