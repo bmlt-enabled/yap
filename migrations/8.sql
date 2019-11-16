c453a63eba7860595cb5e32131ae8298d01ab0dd
Danny <danny.gershman@gmail.com>
2019-11-15 23:29:06 -0500

    schema support for mysql 5.5

--- migrations/8.sql
+++ migrations/8.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `records` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `callsid` varchar(255) NOT NULL,
-  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `end_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `start_time` timestamp NOT NULL,
+  `end_time` timestamp NOT NULL,
   `from_number` varchar(255) NOT NULL,
   `to_number` varchar(255) NOT NULL,
   `payload` longtext,
