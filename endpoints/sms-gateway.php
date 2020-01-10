c5e4595ae4c3e16ce40514f1526107f5a312ad27
spiritualtheory <spiritual.theory@gmail.com>
2020-01-10 12:55:53 -0500

    Fix auto getProvince on sms-gateway meeting search (#369)
    * Fix auto getProvince on sms-gateway meeting search
    
    Added check for comma in last 4 string positions of address
    
    * Fix auto getProvince on sms-gateway meeting search
    
    Added check for comma in last 4 string positions of address

--- endpoints/sms-gateway.php
+++ endpoints/sms-gateway.php
@@ -5,7 +5,11 @@ header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 $address = $_REQUEST['Body'];
-$coordinates = getCoordinatesForAddress($address . "," . getProvince());
+if (str_exists($address, ',')) {
+    $coordinates = getCoordinatesForAddress($address);
+} else {
+    $coordinates = getCoordinatesForAddress($address . "," . getProvince());
+}
 ?>
 <Response>
 <?php
