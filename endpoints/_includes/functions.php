874481f685b12c80906c20ed2e5958f54376bc4c
Danny <danny.gershman@gmail.com>
2020-01-31 09:10:17 -0500

    WIP: voicemail playback

--- endpoints/_includes/functions.php
+++ endpoints/_includes/functions.php
@@ -7,7 +7,7 @@ require_once(!getenv("ENVIRONMENT") ? __DIR__ . '/../../config.php' : __DIR__ . 
 require_once 'migrations.php';
 require_once 'queries.php';
 require_once 'logging.php';
-static $version  = "3.6.3";
+static $version  = "3.7.0";
 static $settings_whitelist = [
     'announce_servicebody_volunteer_routing' => [ 'description' => '' , 'default' => false, 'overridable' => true, 'hidden' => false],
     'blocklist' => [ 'description' => 'Allows for blocking a specific list of phone numbers https://github.com/bmlt-enabled/yap/wiki/Blocklist' , 'default' => '', 'overridable' => true, 'hidden' => false],
@@ -17,7 +17,7 @@ static $settings_whitelist = [
     'config' => [ 'description' => '' , 'default' => null, 'overridable' => true, 'hidden' => true],
     'custom_css' => [ 'description' => '' , 'default' => 'td { font-size: 36px; }', 'overridable' => true, 'hidden' => false],
     'custom_query' => ['description' => '', 'default' => '&sort_results_by_distance=1&long_val={LONGITUDE}&lat_val={LATITUDE}&geo_width={SETTING_MEETING_SEARCH_RADIUS}&weekdays={DAY}', 'overridable' => true, 'hidden' => false],
-    'digit_map_search_type' => [ 'description' => '', 'default' => ['1' => SearchType::VOLUNTEERS, '2' => SearchType::MEETINGS, '3' => SearchType::JFT, '9' => SearchType::DIALBACK], 'overridable' => true, 'hidden' => false],
+    'digit_map_search_type' => [ 'description' => '', 'default' => ['1' => SearchType::VOLUNTEERS, '2' => SearchType::MEETINGS, '3' => SearchType::JFT, '8' => SearchType::VOICEMAIL_PLAYBACK, '9' => SearchType::DIALBACK], 'overridable' => true, 'hidden' => false],
     'digit_map_location_search_method' => [ 'description' => '', 'default' => ['1' => LocationSearchMethod::VOICE, '2' => LocationSearchMethod::DTMF, '3' => SearchType::JFT], 'overridable' => true, 'hidden' => false],
     'extension_dial' => [ 'description' => '', 'default' => false, 'overridable' => true, 'hidden' => false],
     'fallback_number' => [ 'description' => '' , 'default' => '', 'overridable' => true, 'hidden' => false],
@@ -102,6 +102,7 @@ class SearchType
     const VOLUNTEERS = 1;
     const MEETINGS = 2;
     const JFT = 3;
+    const VOICEMAIL_PLAYBACK = 999;
     const DIALBACK = 1000;
 }
 
@@ -127,6 +128,8 @@ class EventId
     const CALLER_HUP = 13;
     const MEETING_SEARCH_LOCATION_GATHERED = 14;
     const HELPLINE_ROUTE = 15;
+    const VOICEMAIL_PLAYBACK = 16;
+    const DIALBACK = 17;
 
     public static function getEventById($id)
     {
@@ -161,6 +164,10 @@ class EventId
                 return "Meeting Search Location Gathered";
             case self::HELPLINE_ROUTE:
                 return "Helpline Route";
+            case self::VOICEMAIL_PLAYBACK:
+                return "Voicemail Playback";
+            case self::DIALBACK:
+                return "Dialback";
         }
     }
 }
---
+++ endpoints/voicemail-playback.php
@@ -0,0 +1,31 @@
+<?php
+require_once '_includes/functions.php';
+header("content-type: text/xml");
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+// TODO: Service body ID hardcoded.
+$voicemails = getVoicemail(101);
+insertCallEventRecord(EventId::VOICEMAIL_PLAYBACK);
+
+// TODO: How to determine which service body ID to use?
+// TODO: Who can play voicemails play?
+// TODO: How many voicemails to play?
+// TODO: Date and phone number formatting
+
+if (count($voicemails) > 0) {
+    $voicemail = $voicemails[0];
+?>
+<Response>
+    <Say voice="<?php echo voice(); ?>" language="<?php echo setting('language') ?>">
+        <?php echo sprintf("Voicemail received at %s, from phone number %s", $voicemail['event_time'], $voicemail['from_number'])?>
+    </Say>
+    <Play><?php echo sprintf("%s.%s", json_decode($voicemail['meta'])->url,'mp3')?></Play>
+    <Hangup/>
+</Response>
+<?php } else { ?>
+<Response>
+    <Say voice="<?php echo voice(); ?>" language="<?php echo setting('language') ?>">
+        There are no recent voicemail messages to play.
+    </Say>
+    <Hangup/>
+</Response>
+<?php }
