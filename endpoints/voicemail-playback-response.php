1666933850d0db6727c0fb76509a35c3b389de6a
Danny <danny.gershman@gmail.com>
2020-02-01 01:00:22 -0500

    WIP: date and phone number formatting for say, pauses

--- endpoints/voicemail-playback-response.php
+++ endpoints/voicemail-playback-response.php
@@ -9,27 +9,34 @@ $service_body_obj = getServiceBodyCoverage($coordinates->latitude, $coordinates-
 $service_body_id = isset($service_body_obj) ? $service_body_obj->id : 0;
 $location = $service_body_obj->name;
 $voicemails = getVoicemail($service_body_id);
+$voicemail_grace_hrs = 600;  // TODO: hardcoded to 48 hrs, convert to a setting?
 insertCallEventRecord(EventId::VOICEMAIL_PLAYBACK);
 
 // TODO: Who can play voicemails play?
-// TODO: How many voicemails to play? (last 48 hours)
-// TODO: Date and phone number formatting
 ?>
 <Response>
     <Say voice="<?php echo voice(); ?>" language="<?php echo setting('language') ?>">
         <?php echo sprintf("Connected to %s voicemail", $location);?>
     </Say>
+    <Pause length="2" />
 <?php
 if (count($voicemails) > 0) {
-    $voicemail = $voicemails[0];?>
+    foreach ($voicemails as $voicemail) {
+        if (strtotime($voicemail['event_time']) > (new DateTime())->modify(sprintf("-%s hours", $voicemail_grace_hrs))->getTimestamp()) {?>
     <Say voice="<?php echo voice(); ?>" language="<?php echo setting('language') ?>">
-        <?php echo sprintf("Voicemail received at %s, from phone number %s", $voicemail['event_time'], $voicemail['from_number'])?>
+        <?php echo sprintf("Voicemail received at %s, from phone number %s", date("l, F dS, yy, h:m A", strtotime($voicemail['event_time'])), implode(' ', str_split(str_replace('+', '', $voicemail['from_number'])))) ?>
     </Say>
-    <Play><?php echo sprintf("%s.%s", json_decode($voicemail['meta'])->url, 'mp3')?></Play>
-<?php } else { ?>
+    <Pause length="2"/>
+    <Play><?php echo sprintf("%s.%s", json_decode($voicemail['meta'])->url, 'mp3') ?></Play>
+    <Pause length="2"/>
+    <?php
+        }
+    }
+} else { ?>
     <Say voice="<?php echo voice(); ?>" language="<?php echo setting('language') ?>">
         There are no recent voicemail messages to play.
     </Say>
+    <Pause length="2"/>
 <?php }?>
     <Hangup/>
 </Response>
