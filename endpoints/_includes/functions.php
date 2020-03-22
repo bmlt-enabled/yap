f975d181202617c7e119c06ec813ab07d29cd3d9
Danny <danny.gershman@gmail.com>
2020-03-22 13:46:43 -0400

    removing bmlt as required fields

--- endpoints/_includes/functions.php
+++ endpoints/_includes/functions.php
@@ -431,8 +431,6 @@ class UpgradeAdvisor
         'google_maps_api_key',
         'twilio_account_sid',
         'twilio_auth_token',
-        'bmlt_username',
-        'bmlt_password',
         'mysql_hostname',
         'mysql_username',
         'mysql_password',
