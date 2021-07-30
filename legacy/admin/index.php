69764c262bc6dd15aaaf9397d890336f56c96682
Danny Gershman <danny.gershman@gmail.com>
2021-07-29 22:24:47 -0400

    fix redirect after installer complete (#525)

--- legacy/admin/index.php
+++ legacy/admin/index.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'header.php';
-if ($_SERVER['REQUEST_URI'] == "/admin") {
-    header('Location: /admin/', null, 301);
-};
+if (stripos($_SERVER['REQUEST_URI'], '/admin/index.php') == true) {
+    header('Location: /admin', null, 301);
+}
 
 $state = UpgradeAdvisor::getState();
 ?>
@@ -20,7 +20,7 @@ $state = UpgradeAdvisor::getState();
     <div class="custom-control custom-switch" style="display: none;">
         <input type="checkbox" class="custom-control-input" id="darkSwitch" />
     </div>
-    <form id="auth" class="form-signin" method="POST" action="admin/auth_login.php">
+    <form id="auth" class="form-signin" method="POST" action="auth_login.php">
         <div id="admin_title"><?php echo isset($GLOBALS['admin_title']) ? $GLOBALS['admin_title'] : ""; ?></div>
         <div id="yap-logo"></div>
         <div id="no-auth-message">
