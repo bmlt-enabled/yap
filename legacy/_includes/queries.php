4443bdaa37eecde2e9f987937411ac603e7f61c4
Danny Gershman <danny.gershman@gmail.com>
2022-07-23 22:14:06 -0400

    completed query fixes

--- legacy/_includes/queries.php
+++ legacy/_includes/queries.php
@@ -69,12 +69,23 @@ function quickExec($sql)
 function getCallRecords($service_body_ids, $date_range_start, $date_range_end)
 {
     $guid = uniqid();
-    quickExec("INSERT INTO `cache_records_conference_participants` SELECT DISTINCT r.callsid as parent_callsid,cp2.callsid,'".$guid."' as guid FROM records r LEFT OUTER JOIN conference_participants cp ON r.callsid = cp.callsid OR cp.callsid IS NULL LEFT OUTER JOIN conference_participants cp2 ON cp.conferencesid = cp2.conferencesid WHERE r.start_time >= '$date_range_start' AND r.start_time <= '$date_range_end';");
+    quickExec("INSERT INTO `cache_records_conference_participants`
+SELECT DISTINCT r.callsid as parent_callsid,cp2.callsid,'".$guid."' as guid,re.service_body_id
+FROM records r LEFT OUTER JOIN conference_participants cp ON r.callsid = cp.callsid OR cp.callsid IS NULL
+LEFT OUTER JOIN conference_participants cp2 ON cp.conferencesid = cp2.conferencesid
+LEFT OUTER JOIN records_events re ON cp.callsid = re.callsid AND re.service_body_id is not null
+WHERE r.start_time >= '$date_range_start' AND r.start_time <= '$date_range_end'
+UNION
+SELECT DISTINCT r.callsid as parent_callsid,r.callsid,'".$guid."' as guid,re.service_body_id
+FROM records r LEFT OUTER JOIN records_events re on r.callsid = re.callsid AND re.service_body_id IS not NULL
+WHERE r.start_time >= '$date_range_start' AND r.start_time <= '$date_range_end'");
 
     $db = new Database();
     $sql = sprintf("SELECT r.`id`,CONCAT(r.`start_time`, 'Z') as start_time,CONCAT(r.`end_time`, 'Z') as end_time,r.`duration`,r.`from_number`,r.`to_number`,r.`callsid`,re.`service_body_id`,
 CONCAT('[', GROUP_CONCAT('{\"meta\":', IFNULL(re.meta, '{}'), ',\"event_id\":', re.event_id, ',\"event_time\":\"', re.event_time, 'Z\",\"service_body_id\":', COALESCE(re.service_body_id, 0), '}' ORDER BY re.event_time DESC SEPARATOR ','), ']') as call_events
-FROM records_events re
+FROM (SELECT ire.id,ire.callsid, ire.event_time,ire.event_id,ircp.service_body_id,meta FROM records_events ire
+      left outer join cache_records_conference_participants ircp ON ire.callsid = ircp.callsid
+      where guid = :guid) re
 INNER JOIN cache_records_conference_participants rcp ON rcp.parent_callsid = re.callsid
 INNER JOIN records r ON r.callsid = rcp.parent_callsid
 WHERE re.service_body_id in (%s) AND rcp.guid = :guid
