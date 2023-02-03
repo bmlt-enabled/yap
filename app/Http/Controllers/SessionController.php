1d71d9e866b42b3813eedc48c921a964be4413a0
Danny Gershman <danny.gershman@gmail.com>
2023-02-02 22:01:34 -0500

    updating session purge

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -6,19 +6,16 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-    public function delete()
+    public function delete(Request $request)
     {
         if (!isset($_SESSION)) {
             session_start();
         }
-        foreach ($_SESSION as $key => $value) {
-            if (strpos($key, "cache_") === 0
-                || strpos($key, "override_") === 0
-                || strpos($key, 'call_state') === 0) {
-                echo sprintf("Cleared <i>%s</i><br/>", $key);
-                unset($_SESSION[$key]);
-            }
-        }
-        return response("OK")->header('Content-Type', 'text/plain');
+        $cookie = cookie(session_id(), "", time() - 3600);
+        session_destroy();
+        session_write_close();
+        return response("OK")
+            ->header('Content-Type', 'text/plain')
+            ->withCookie($cookie);
     }
 }
