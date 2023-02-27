b5fc9a914a26d9a2b009234674d62b26a57875b9
Danny Gershman <danny.gershman@gmail.com>
2023-02-26 22:25:52 -0500

    adding mui and menubar

---
+++ resources/js/components/MenuBar.js
@@ -0,0 +1,83 @@
+import {useState} from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import { Link } from "react-router-dom";
+import {Divider, Drawer, IconButton, ListItemButton, Typography} from "@mui/material";
+import {Menu, Close} from "@mui/icons-material";
+
+function MenuBar() {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+
+    const toggleDrawer = (open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setIsDrawerOpen(open)
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2, display: { xs: 'block', sm: 'none'}}}
+                        onClick={() => setIsDrawerOpen(true)}>
+                        <Menu />
+                    </IconButton>
+                    <Drawer open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+                        <Box sx={{
+                            p: 2,
+                            height: 1,
+                        }}>
+                            <IconButton sx={{mb: 2, color: "white"}}>
+                                <Close onClick={toggleDrawer(false)} />
+                            </IconButton>
+                            <Divider sx={{mb: 2}} />
+                            <Box sx={{mb: 2}}>
+                                <Link to="/" style={{textDecoration:'none'}}>
+                                    <ListItemButton
+                                        onClick={toggleDrawer(false)}>Home</ListItemButton>
+                                </Link>
+                            </Box>
+                        </Box>
+                    </Drawer>
+                    <Box>
+                        <Typography>Yap</Typography>
+                    </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex' } }}>
+                        <Link to="/" style={{textDecoration:'none'}}>
+                            <Button color="secondary" variant="text">Home</Button>
+                        </Link>
+                        <Link to="/reports" style={{textDecoration:'none'}}>
+                            <Button color="secondary" variant="text">Reports</Button>
+                        </Link>
+                        <Link to="/serviceBodies" style={{textDecoration:'none'}}>
+                            <Button color="secondary" variant="text">Service Bodies</Button>
+                        </Link>
+                        <Link to="/schedules" style={{textDecoration:'none'}}>
+                            <Button color="secondary" variant="text">Schedules</Button>
+                        </Link>
+                        <Link to="/volunteers" style={{textDecoration:'none'}}>
+                            <Button color="secondary" variant="text">Volunteers</Button>
+                        </Link>
+                        <Link to="/groups" style={{textDecoration:'none'}}>
+                            <Button color="secondary" variant="text">Groups</Button>
+                        </Link>
+                        <Link to="users" style={{textDecoration:'none'}}>
+                            <Button color="secondary" variant="text">Users</Button>
+                        </Link>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default MenuBar;
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ])
+mix.js('resources/js/index.js', 'public/js')
     .react()
