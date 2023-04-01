b6c2f77b69e8c58c3c01c0e54a813c1ec0aa5980
Danny Gershman <danny.gershman@gmail.com>
2023-03-31 22:06:39 -0400

    better formatting

--- resources/js/components/VolunteerControl.js
+++ resources/js/components/VolunteerControl.js
@@ -1,10 +1,13 @@
 import {useEffect, useState} from "react";
+import Box from "@mui/material/Box";
+import {Card, TextField} from "@mui/material";
 
-function VolunteerControl({serviceBodyId}) {
+function VolunteerControl({serviceBodyId})
+{
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    const getVolunteers = async () => {
+    const getVolunteers = async() => {
         setLoading(true)
         let response = await fetch(`${rootUrl}/api/v1/config?service_body_id=${serviceBodyId}&data_type=_YAP_VOLUNTEERS_V2_`)
         let responseData = await response.json()
@@ -13,13 +16,20 @@ function VolunteerControl({serviceBodyId}) {
     }
 
     useEffect(() => {
-        console.log(`volutneercontrol: ${serviceBodyId}`)
+        console.log(`volunteercontrol: ${serviceBodyId}`)
         getVolunteers()
     }, [])
 
     return (
-        !loading && data != null ? JSON.stringify(data) : ""
-    )
+        !loading && data != null ? data.data.map(volunteer => (
+            <Card>
+                <div>
+                    <TextField label="Volunteer Name" defaultValue={volunteer.volunteer_name}/>
+                    <TextField label="Phone Number" defaultValue={volunteer.volunteer_phone_number}/>
+                </div>
+            </Card>
+        )) : ""
+    );
 }
 
 export default VolunteerControl;
