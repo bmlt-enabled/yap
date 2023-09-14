470ed3fbb7f4e6a4682babcceb44d0d3a748d55c
Danny Gershman <danny.gershman@gmail.com>
2023-09-13 23:06:12 -0500

    adding waiting message

--- app/Http/Controllers/HelplineController.php
+++ app/Http/Controllers/HelplineController.php
@@ -186,7 +186,7 @@ class HelplineController extends Controller
                         ->setNumDigits(1)
                         ->setAction(("helpline-search.php?CaptchaVerified=1&ForceNumber="
                         . urlencode($request->get('ForceNumber'))
-                        . $this->settings->getSessionLink(true) . " " . ($waiting_message ? "&amp;WaitingMessage=1" : "")));
+                        . $this->settings->getSessionLink(true) . ($waiting_message ? "&WaitingMessage=1" : "")));
 
                     $gather->say($this->settings->get('title') .  "..." . $this->settings->word('press_any_key_to_continue'))
                         ->setVoice($this->settings->voice())
