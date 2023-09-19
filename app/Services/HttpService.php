704d7b31e7fab30b6a003492915d415f9f5d850d
Danny Gershman <danny.gershman@gmail.com>
2023-09-19 00:58:23 -0500

    more tests 3

--- app/Services/HttpService.php
+++ app/Services/HttpService.php
@@ -7,13 +7,6 @@ use Illuminate\Support\Facades\Http;
 
 class HttpService
 {
-    protected SettingsService $settings;
-
-    public function __construct(SettingsService $settings)
-    {
-        $this->settings = $settings;
-    }
-
     const USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 +yap";
 
     public function get($url, $ttl = 0, $extraHeaders = []) : string
--- tests/Feature/MeetingSearchTest.php
+++ tests/Feature/MeetingSearchTest.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Models\MeetingResults;
 use App\Models\Timezone;
+use App\Services\HttpService;
+use App\Services\MeetingResultsService;
 use App\Services\SettingsService;
 use App\Services\TimeZoneService;
 use App\Services\TwilioService;
 use Tests\FakeTwilioHttpClient;
-use Twilio\Rest\Client;
 
 beforeAll(function () {
     putenv("ENVIRONMENT=test");
@@ -33,6 +35,57 @@ beforeEach(function () {
     $this->longitude = '-78.6382';
 });
 
+test('meeting search with a failure on BMLT server exception', function ($method) {
+    $settingsService = new SettingsService();
+    app()->instance(SettingsService::class, $settingsService);
+    $http = mock('App\Services\HttpService')->makePartial();
+    $http->shouldReceive("get")->withAnyArgs()->andThrow(Exception::class);
+    app()->instance(HttpService::class, $http);
+
+    $response = $this->call($method, '/meeting-search.php', [
+        "Latitude" => 0,
+        "Longitude" => 0,
+    ]);
+    $response
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "text/xml; charset=UTF-8")
+        ->assertSeeInOrder([
+            '<?xml version="1.0" encoding="UTF-8"?>',
+            '<Response><Redirect method="GET">fallback.php</Redirect></Response>',
+        ], false);
+})->with(['GET', 'POST']);
+
+test('meeting search with no more results today', function ($method) {
+    $settingsService = new SettingsService();
+    app()->instance(SettingsService::class, $settingsService);
+
+    $meetingResults = new MeetingResults();
+    $meetingResults->originalListCount = 1;
+
+    $meetingResultsService = mock('\App\Services\MeetingResultsService')->makePartial();
+    $meetingResultsService->shouldReceive('getMeetings')
+        ->withAnyArgs()->once()->andReturn($meetingResults);
+
+    app()->instance(MeetingResultsService::class, $meetingResultsService);
+
+    $response = $this->call($method, '/meeting-search.php', [
+        "Latitude" => 0,
+        "Longitude" => 0,
+    ]);
+    $response
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "text/xml; charset=UTF-8")
+        ->assertSeeInOrder([
+            '<?xml version="1.0" encoding="UTF-8"?>',
+            '<Response>',
+            '<Say voice="alice" language="en-US">',
+            'there are no other meetings for today..... try again',
+            '</Say>',
+            '<Redirect method="GET">input-method.php?Digits=2</Redirect>',
+            '</Response>',
+        ], false);
+})->with(['GET', 'POST']);
+
 test('meeting search with odd coordinates on meeting lookup', function ($method) {
     $response = $this->call($method, '/meeting-search.php', [
         "Latitude" => 0,
@@ -47,6 +100,47 @@ test('meeting search with odd coordinates on meeting lookup', function ($method)
             '<Say voice="alice" language="en-US">no results found... you might have an invalid entry... try again</Say>',
             '<Redirect method="GET">input-method.php?Digits=2</Redirect>',
             '</Response>',
+        ], false);
+})->with(['GET', 'POST']);
+
+test('meeting search with valid latitude and longitude suppressing voice results', function ($method) {
+    $settingsService = new SettingsService();
+    $settingsService->set("suppress_voice_results", true);
+    app()->instance(SettingsService::class, $settingsService);
+    app()->instance(TwilioService::class, $this->twilioService);
+    $this->twilioService->shouldReceive("client")->withArgs([])->andReturn($this->twilioClient);
+    $this->twilioService->shouldReceive("settings")->andReturn($settingsService);
+
+    $timezone = new Timezone('OK', 0, -18000, 'America/New_York', 'Eastern Standard Time');
+    $timezoneService = mock(TimeZoneService::class)->makePartial();
+    $timezoneService->shouldReceive('getTimeZoneForCoordinates')
+        ->withArgs([$this->latitude, $this->longitude])
+        ->once()
+        ->andReturn($timezone);
+    app()->instance(TimeZoneService::class, $timezoneService);
+
+    // mocking TwilioRestClient->messages->create()
+    $messageListMock = mock('\Twilio\Rest\Api\V2010\Account\MessageList');
+    $messageListMock->shouldReceive('create')
+        ->with(is_string(""), is_array([]))->times(5);
+    $this->twilioClient->messages = $messageListMock;
+
+    $response = $this->call($method, '/meeting-search.php', [
+        'Latitude' => $this->latitude,
+        'Longitude' => $this->longitude
+    ]);
+
+    $response
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "text/xml; charset=UTF-8")
+        ->assertSee([
+            '<?xml version="1.0" encoding="UTF-8"?>',
+            '<Response>',
+            '<Say voice="alice" language="en-US">5 meetings have been texted to you</Say>',
+            '<Say voice="alice" language="en-US">Meeting search results will also be sent to you by SMS text message.</Say>',
+            '<Pause length="2"/>',
+            '<Say voice="alice" language="en-US">thank you for calling, goodbye</Say>',
+            '</Response>'
         ], false);
 })->with(['GET', 'POST']);
 
