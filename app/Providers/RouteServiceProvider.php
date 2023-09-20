0cda3a0fe258bbf0d800ae58f2befa6b42517912
Danny Gershman <danny.gershman@gmail.com>
2023-09-19 22:37:10 -0500

    removing unused stub controller

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -43,10 +43,6 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
-            Route::prefix('stub')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/stub.php'));
-
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
--- routes/stub.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get("/timezone", 'App\Http\Controllers\StubsController@timezone');
-Route::get("/geocode", 'App\Http\Controllers\StubsController@geocode');
