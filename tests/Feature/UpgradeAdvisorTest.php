0f8eff0f0d81f3a62a06d0ea16e083fcefd10ba4
Danny Gershman <danny.gershman@gmail.com>
2024-01-18 22:45:51 -0500

    Feature/upgrade advisor alert fix (#913)

--- tests/Feature/UpgradeAdvisorTest.php
+++ tests/Feature/UpgradeAdvisorTest.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Constants\AlertId;
+use App\Repositories\ReportsRepository;
+use App\Services\DatabaseMigrationsService;
+use App\Services\GeocodingService;
 use App\Services\SettingsService;
+use App\Services\TimeZoneService;
+use App\Services\TwilioService;
+use Tests\FakeTwilioHttpClient;
 
 beforeAll(function () {
     putenv("ENVIRONMENT=test");
@@ -11,6 +18,14 @@ beforeEach(function () {
     $_SERVER['REQUEST_URI'] = "/";
     $_REQUEST = null;
     $_SESSION = null;
+
+    $fakeHttpClient = new FakeTwilioHttpClient();
+    $this->twilioClient = mock('Twilio\Rest\Client', [
+        "username" => "fake",
+        "password" => "fake",
+        "httpClient" => $fakeHttpClient
+    ])->makePartial();
+    $this->twilioService = mock(TwilioService::class)->makePartial();
 });
 
 test('version test', function ($method) {
@@ -25,7 +40,6 @@ test('version test', function ($method) {
             'version' => $settings->version(),
         ]);
 })->with(['GET', 'POST']);
-;
 
 test('version test as jsonp', function ($method) {
     $settings = new SettingsService();
@@ -36,7 +50,67 @@ test('version test as jsonp', function ($method) {
         ->assertHeader("Content-Type", "application/javascript")
         ->assertSeeText(sprintf("bro({\"version\":\"%s\"})", $settings->version()), false);
 })->with(['GET', 'POST']);
-;
+
+test('test with misconfigured phone number', function ($method) {
+    $misconfiguredNumber = "+18889822614";
+    $settingsService = new SettingsService();
+    $this->twilioService->shouldReceive("client")->withArgs([])->andReturn($this->twilioClient);
+    $this->twilioService->shouldReceive("settings")->andReturn($settingsService);
+    $geocodingService = mock(GeocodingService::class)->makePartial();
+    app()->instance(SettingsService::class, instance: $settingsService);
+
+    $geocodingService
+        ->shouldReceive("ping")
+        ->withArgs(["91409"])
+        ->andReturn((object)['status' => 'OK'])
+        ->once();
+    app()->instance(GeocodingService::class, $geocodingService);
+
+    $timezoneService = mock(TimeZoneService::class)->makePartial();
+    $timezoneService
+        ->shouldReceive("getTimeZoneForCoordinates")
+        ->withAnyArgs()
+        ->andReturn((object)['status' => 'OK'])
+        ->once();
+    app()->instance(TimeZoneService::class, $timezoneService);
+
+    $reportsRepository = mock(ReportsRepository::class)->makePartial();
+    $reportsRepository
+        ->shouldReceive("getMisconfiguredPhoneNumbersAlerts")
+        ->withArgs([AlertId::STATUS_CALLBACK_MISSING])
+        ->andReturn([(object)[
+            'payload'=>$misconfiguredNumber
+        ]])
+        ->once();
+    app()->instance(ReportsRepository::class, $reportsRepository);
+
+    $this->twilioService->client()->shouldReceive('getAccountSid')->andReturn("123");
+    $incomingPhoneNumberContext = mock('\Twilio\Rest\Api\V2010\Account\InstanceContext');
+    $incomingPhoneNumberInstance= mock('\Twilio\Rest\Api\V2010\Account\IncomingPhoneNumberInstance');
+    $incomingPhoneNumberInstance->voiceUrl = "http://localhost:3100/yap/index.php";
+    $incomingPhoneNumberContext->shouldReceive('read')->withNoArgs()
+        ->andReturn([$incomingPhoneNumberInstance])->once();
+
+    // mocking TwilioRestClient->incomingPhoneNumbers->read();
+    $this->twilioService->client()->incomingPhoneNumbers = $incomingPhoneNumberContext;
+
+    app()->instance(TwilioService::class, $this->twilioService);
+
+    $response = $this->call($method, '/upgrade-advisor.php');
+    $response
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "application/json")
+        ->assertJson(
+            [
+                "status"=>true,
+                "message"=>"Ready To Yap!",
+                "warnings"=>sprintf("%s is/are phone numbers that are missing Twilio Call Status Changes Callback status.php webhook. This will not allow call reporting to work correctly.  For more information review the documentation page https://github.com/bmlt-enabled/yap/wiki/Call-Detail-Records.", $misconfiguredNumber),
+                "version"=>$settingsService->version(),
+                "db"=>100,
+                "build"=>"local"
+            ]
+        );
+})->with(['GET', 'POST']);
 
 //test('bad google maps api key', function () {
 //    $settings = new SettingsService();
