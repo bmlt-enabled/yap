419907bb23ceda71ec0fa3cb77d12da39fe1edad
Danny Gershman <danny.gershman@gmail.com>
2024-04-01 10:25:53 -0400

    working changes for running tests in docker

--- config.test.php
+++ config.test.php
@@ -1,7 +1,7 @@
 <?php
 static $title = "Test Helpline";
 static $bmlt_root_server = "https://latest.aws.bmlt.app/main_server";
-static $mysql_hostname = "127.0.0.1";
+static $mysql_hostname = "host.docker.internal";
 static $mysql_username = "root";
 static $mysql_password = "yap_root_password";
 static $mysql_database = "yap_test";
--- cypress/e2e/authentication/login.cy.js
+++ cypress/e2e/authentication/login.cy.js
@@ -2,16 +2,16 @@ describe('Login', () => {
 
     beforeEach(() => {
         // runs before each test in the block
-        cy.visit('http://localhost:3100/yap/admin')
+        cy.visit('/admin')
     });
 
     it('Login with creds', () => {
         cy.get("#inputUsername").focus();
-        cy.get("#inputUsername").type("admin");
+        cy.get("#inputUsername").type("gnyr_admin");
         cy.get("#inputPassword").focus()
-        cy.get("#inputPassword").type("admin");
+        cy.get("#inputPassword").type("CoreysGoryStory");
         cy.get("#authenticateButton").contains("Authenticate").click();
 
-        cy.get('.home-title').contains("Welcome, admin...");
+        cy.get('.home-title').contains("Welcome, gnyr_admin...");
     })
 })
