d9f02ab25329523fbc01168559d583a411e7469a
Danny Gershman <danny.gershman@gmail.com>
2024-04-03 01:42:36 -0400

    use yap test database

--- src/config.test.php
+++ src/config.test.php
@@ -4,7 +4,7 @@ static $bmlt_root_server = "https://latest.aws.bmlt.app/main_server";
 static $mysql_hostname = "127.0.0.1";
 static $mysql_username = "root";
 static $mysql_password = "yap_root_password";
-static $mysql_database = "yap";
+static $mysql_database = "yap_test";
 static $mysql_port = 3106;
 #https://www.twilio.com/docs/iam/test-credentials
 static $twilio_account_sid = "AC222a79bf52fdc8c3cf463b2846582b83";
