4456580689441a6e89ae8f861cf40a2a4f4774e9
Danny Gershman <danny.gershman@gmail.com>
2024-07-01 02:03:42 -0400

    wip

--- src/tests/MiddlewareTests.php
+++ src/tests/MiddlewareTests.php
@@ -2,15 +2,7 @@
 
 namespace Tests;
 
-use App\Repositories\ReportsRepository;
-use Mockery;
-
 class MiddlewareTests
 {
-    public function insertSession($callSid) : Mockery\MockInterface
-    {
-        $repository = Mockery::mock(ReportsRepository::class)->makePartial();
-        $repository->shouldReceive('insertSession')->with($callSid);
-        return $repository;
-    }
+
 }
