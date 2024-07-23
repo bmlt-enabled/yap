ea02ef4042d6fa44c26589a6c46a823f41e78374
Danny Gershman <danny.gershman@gmail.com>
2024-07-22 22:53:55 -0400

    some incremental changes

--- src/resources/js/pages/Schedules.js
+++ src/resources/js/pages/Schedules.js
@@ -1,6 +1,27 @@
+import timeGridPlugin from '@fullcalendar/timegrid';
+import FullCalendar from "@fullcalendar/react";
+import {useState} from "react";
+import ServiceBodiesDropdown from "../components/ServiceBodiesDropdown";
+
 function Schedules() {
+    const [eventData, setEventData] = useState([]);
+
+    const serviceBodyOnChange = async(id) => {
+        let response = await fetch(`${rootUrl}/api/v1/volunteers/schedule?service_body_id=${id}`)
+        let responseData = await response.json()
+        setEventData(responseData)
+    }
+
     return (
-        <h1>Schedules</h1>
+        <div>
+            <ServiceBodiesDropdown handleChange={serviceBodyOnChange} />
+            { }
+            <FullCalendar
+                plugins={[ timeGridPlugin ]}
+                initialView="timeGridWeek"
+                events={eventData}
+            />
+        </div>
     )
 }
 
