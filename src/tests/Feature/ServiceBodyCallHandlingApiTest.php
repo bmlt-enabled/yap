2cf4aef509df8898fd3af0a65ab9de7718b2a650
Danny Gershman <danny.gershman@gmail.com>
2024-10-26 14:46:54 -0400

    update service body call handling test

--- src/tests/Feature/ServiceBodyCallHandlingApiTest.php
+++ src/tests/Feature/ServiceBodyCallHandlingApiTest.php
@@ -79,3 +79,43 @@ test('get call handling for a service body that does not exist', function () {
         ->assertHeader("Content-Type", "application/json")
         ->assertStatus(200);
 });
+
+test('update call handling for a service body', function () {
+    $_SESSION['auth_mechanism'] = AuthMechanism::V2;
+    $callHandling = new ServiceBodyCallHandling();
+    $callHandling->forced_caller_id_number = "123";
+
+    $response = $this->call(
+        'POST',
+        '/api/v1/callHandling',
+        ['serviceBodyId' => $this->serviceBodyId],
+        content: json_encode($callHandling)
+    );
+
+    $response->assertJson([
+        "id"=>88,
+        "parent_id"=>0,
+        "service_body_id"=>intval($this->serviceBodyId),
+        "data"=>[$callHandling->toArray()]])
+        ->assertHeader("Content-Type", "application/json")
+        ->assertStatus(200);
+
+
+    $callHandling = new ServiceBodyCallHandling();
+    $callHandling->forced_caller_id_number = "123abc";
+
+    $response = $this->call(
+        'POST',
+        '/api/v1/callHandling',
+        ['serviceBodyId' => $this->serviceBodyId],
+        content: json_encode($callHandling)
+    );
+
+    $response->assertJson([
+        "id"=>88,
+        "parent_id"=>0,
+        "service_body_id"=>intval($this->serviceBodyId),
+        "data"=>[$callHandling->toArray()]])
+        ->assertHeader("Content-Type", "application/json")
+        ->assertStatus(200);
+});
