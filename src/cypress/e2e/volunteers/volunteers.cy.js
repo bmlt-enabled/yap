741a5adb5a99d3e6334b477d6e97a3f4c786f441
Danny Gershman <danny.gershman@gmail.com>
2024-10-27 23:22:22 -0400

    volunteers cypress tests

--- src/cypress/e2e/volunteers/volunteers.cy.js
+++ src/cypress/e2e/volunteers/volunteers.cy.js
@@ -27,6 +27,73 @@ describe('Volunteers', () => {
             .get('.navbar-nav')
             .contains('Volunteers')
             .click()
+
+        cy
+            .get("#add-volunteer").click()
+
+        cy
+            .get('#volunteerCard_1 > #volunteersForm > .card-header > .form-group > .volunteer-name-text > #volunteer_name')
+            .invoke('val', 'danny g')
+            .get('#volunteerCard_1 > #volunteersForm > .card-header > .form-group > .expand-button > .btn')
+            .click()
+            .wait(500)
+            .get('#volunteerCard_1 > #volunteersForm > .card-body > :nth-child(1) > #volunteer_phone_number')
+            .invoke('val', '9735558811')
+            .get('#volunteerCard_1 > #volunteersForm > .card-footer > #volunteerCardFooter > .form-check > #volunteer_enabled')
+            .click()
+            .get('#save-volunteers')
+            .click()
+
+        cy.wait(2000)
     });
 
+    it('Check Volunteer', () => {
+        cy
+            .login()
+            .get('.navbar-nav')
+            .contains('Volunteers')
+            .click()
+
+        cy
+            .get('#volunteerCard_1 > #volunteersForm > .card-header > .form-group > .expand-button > .btn')
+            .click()
+            .wait(500)
+            .get('#volunteerCard_1 > #volunteersForm > .card-header > .form-group > .volunteer-name-text > #volunteer_name')
+            .should('have.value', 'danny g')
+
+        cy.wait(2000)
+    });
+
+    it('Update Volunteer', () => {
+        cy
+            .login()
+            .get('.navbar-nav')
+            .contains('Volunteers')
+            .click()
+
+        cy
+            .get('#volunteerCard_1 > #volunteersForm > .card-header > .form-group > .volunteer-name-text > #volunteer_name')
+            .invoke('val', 'danny g2')
+            .get('#volunteerCard_1 > #volunteersForm > .card-header > .form-group > .expand-button > .btn')
+            .click()
+            .get('#save-volunteers')
+            .click()
+
+        cy.wait(2000)
+
+        cy
+            .login()
+            .get('.navbar-nav')
+            .contains('Volunteers')
+            .click()
+
+        cy
+            .get('#volunteerCard_1 > #volunteersForm > .card-header > .form-group > .expand-button > .btn')
+            .click()
+            .wait(500)
+            .get('#volunteerCard_1 > #volunteersForm > .card-header > .form-group > .volunteer-name-text > #volunteer_name')
+            .should('have.value', 'danny g2')
+
+        cy.wait(2000)
+    });
 });
