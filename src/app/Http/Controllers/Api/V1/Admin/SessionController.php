4acd58ae9b30063807d27e147d72b8bc9b2d7b3b
Danny Gershman <danny.gershman@gmail.com>
2024-12-06 15:38:49 -0500

    refactored session delete

--- src/app/Http/Controllers/Api/V1/Admin/SessionController.php
+++ src/app/Http/Controllers/Api/V1/Admin/SessionController.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1\Admin;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-    public function delete(Request $request)
+    public function store()
     {
         foreach (Session::all() as $key => $value) {
             if (str_starts_with($key, "cache_") || str_starts_with($key, "override_") || str_starts_with($key, "call_state")) {
