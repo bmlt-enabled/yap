0ae3c55444ef1dcf8c8f047182a6972235a9d1ec
Danny Gershman <danny.gershman@gmail.com>
2024-12-14 09:12:31 -0500

    wip logins

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Admin\SwaggerController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
@@ -8,25 +9,28 @@ Route::group([
     'prefix' => 'v1',
     'as' => 'api.',
     'namespace' => 'App\Http\Controllers\Api\V1\Admin',
-    'middleware' => ['authForAdminPortal']
 ], function () {
-    Route::get('/openapi.json', [SwaggerController::class, 'openapi'])->name('openapi');
-    Route::resource('config', 'ConfigController')->only(['index', 'store']);
-    Route::resource('volunteers', 'ConfigureVolunteersController')->only(['index', 'store']);
-    Route::resource('callHandling', 'ServiceBodyCallHandlingController')->only(['index', 'store']);
-    Route::resource('users', 'UserController')->only(['index', 'show', 'store', 'destroy', 'update']);
-    Route::resource('groups', 'GroupController')->only(['index', 'store', 'destroy', 'update']);
-    Route::resource('groups/volunteers', 'GroupVolunteerController')->only(['index', 'store']);
-    Route::resource('volunteers/schedule', 'VolunteerScheduleController')->only(['index']);
-    Route::resource('volunteers/download', 'VolunteerDownloadController')->only(['index']);
-    Route::resource('reports/cdr', 'CdrController')->only(['index']);
-    Route::resource('reports/mapmetrics', 'MapMetricController')->only(['index']);
-    Route::resource('reports/metrics', 'MetricController')->only(['index']);
-    Route::resource('rootServer/servicebodies', 'RootServerServiceBodiesController')->only(['index']);
-    Route::resource('events/status', 'EventStatusController')->only(['index', 'store']);
-    Route::resource('session', 'SessionController')->only(['store']);
-    Route::resource("cache", 'CacheController')->only(['store']);
-    Route::resource('settings', 'SettingsController')->only(['index']);
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+
+    Route::group(['middleware' => ['auth:sanctum']], function() {
+        Route::get('/openapi.json', [SwaggerController::class, 'openapi'])->name('openapi');
+        Route::resource('config', 'ConfigController')->only(['index', 'store']);
+        Route::resource('volunteers', 'ConfigureVolunteersController')->only(['index', 'store']);
+        Route::resource('callHandling', 'ServiceBodyCallHandlingController')->only(['index', 'store']);
+        Route::resource('users', 'UserController')->only(['index', 'show', 'store', 'destroy', 'update']);
+        Route::resource('groups', 'GroupController')->only(['index', 'store', 'destroy', 'update']);
+        Route::resource('groups/volunteers', 'GroupVolunteerController')->only(['index', 'store']);
+        Route::resource('volunteers/schedule', 'VolunteerScheduleController')->only(['index']);
+        Route::resource('volunteers/download', 'VolunteerDownloadController')->only(['index']);
+        Route::resource('reports/cdr', 'CdrController')->only(['index']);
+        Route::resource('reports/mapmetrics', 'MapMetricController')->only(['index']);
+        Route::resource('reports/metrics', 'MetricController')->only(['index']);
+        Route::resource('rootServer/servicebodies', 'RootServerServiceBodiesController')->only(['index']);
+        Route::resource('events/status', 'EventStatusController')->only(['index', 'store']);
+        Route::resource('session', 'SessionController')->only(['store']);
+        Route::resource("cache", 'CacheController')->only(['store']);
+        Route::resource('settings', 'SettingsController')->only(['index']);
+    });
 });
 
 Route::post('/resetDatabase', function () {
