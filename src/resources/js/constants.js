9a91cf5cea537a681675bcd2efac4b205b4effb6
Danny Gershman <danny.gershman@gmail.com>
2025-01-05 14:04:56 -0500

    adding more fields

---
+++ src/resources/js/constants.js
@@ -0,0 +1,7 @@
+// constants.js
+export const VOLUNTEER_ROUTING_OPTIONS = {
+    HELPLINE_FIELD: "helpline_field",
+    VOLUNTEERS: "volunteers",
+    VOLUNTEERS_REDIRECT: "volunteers_redirect",
+    VOLUNTEERS_AND_SMS: "volunteers_and_sms",
+};
