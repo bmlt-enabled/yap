0a06c3b552483529aa6b9475e3a66ca9410b242c
Danny Gershman <danny.gershman@gmail.com>
2025-01-05 16:35:36 -0500

    progress

--- src/resources/js/constants.js
+++ src/resources/js/constants.js
@@ -5,3 +5,16 @@ export const VOLUNTEER_ROUTING_OPTIONS = {
     VOLUNTEERS_REDIRECT: "volunteers_redirect",
     VOLUNTEERS_AND_SMS: "volunteers_and_sms",
 };
+
+export const CALL_STRATEGY = {
+    LINEAR_LOOP_FOREVER: "0",
+    LINEAR_CYCLE_ONCE_THEN_VOICEMAIL: "1",
+    RANDOM_LOOP_FOREVER: "2",
+    BLASTING_THEN_VOICEMAIL: "3",
+    RANDOM_LOOP_ONCE_THEN_VOICEMAIL: "4",
+};
+
+export const SMS_STRATEGY = {
+    RANDOM: "2",
+    BLAST: "3",
+};
