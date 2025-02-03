8cfb5480f28acf39928c4a6d6de778a10a28fb8f
Danny Gershman <danny.gershman@gmail.com>
2025-02-02 22:32:10 -0500

    wip

--- src/resources/js/models/VolunteerModel.js
+++ src/resources/js/models/VolunteerModel.js
@@ -1,6 +1,7 @@
 export const defaultVolunteer = {
     volunteer_enabled: false,
     volunteer_gender: 0,
+    volunteer_responder: 0,
     volunteer_name: "",
     volunteer_notes: "",
     volunteer_phone_number: "",
