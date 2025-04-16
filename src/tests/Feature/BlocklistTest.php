0525a3d2bb625b666cb267517c16a46a7bd36b8b
Danny Gershman <danny.gershman@gmail.com>
2025-04-16 09:46:35 -0400

    blocklist fixes #1329

--- src/tests/Feature/BlocklistTest.php
+++ src/tests/Feature/BlocklistTest.php
@@ -1,4 +1,7 @@
 <?php
+
+use App\Services\SettingsService;
+
 beforeAll(function () {
     putenv("ENVIRONMENT=test");
 });
@@ -11,8 +14,10 @@ beforeEach(function () {
 });
 
 test('test the blocklist with an exact match', function ($method) {
-    $caller = "5557778888";
-    $_SESSION['override_blocklist'] = $caller;
+    $caller = "+15557778888";
+    $settingsService = new SettingsService();
+    $settingsService->set("blocklist", $caller);
+    app()->instance(SettingsService::class, $settingsService);
     $response = $this->call($method, '/', [
         "Caller"=>$caller
     ]);
@@ -22,11 +27,122 @@ test('test the blocklist with an exact match', function ($method) {
         ->assertSeeInOrderExact(["<?xml version='1.0' encoding='UTF-8'?><Response><Reject/></Response>"], false);
 })->with(['GET', 'POST']);
 
+test('test the blocklist with multiple items and an exact match', function ($method) {
+    $caller = "+15557778888,+15557778890";
+    $settingsService = new SettingsService();
+    $settingsService->set("blocklist", $caller);
+    app()->instance(SettingsService::class, $settingsService);
+    $response = $this->call($method, '/', [
+        "Caller"=>"+15557778890"
+    ]);
+    $response
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "text/xml; charset=utf-8")
+        ->assertSeeInOrderExact(["<?xml version='1.0' encoding='UTF-8'?><Response><Reject/></Response>"], false);
+})->with(['GET', 'POST']);
+
 test('test the blocklist without a match', function ($method) {
-    $caller = "5557778888";
-    $_SESSION['override_blocklist'] = $caller;
+    $caller = "+15557778888";
+    $settingsService = new SettingsService();
+    $settingsService->set("blocklist", $caller);
+    app()->instance(SettingsService::class, $settingsService);
     $response = $this->call($method, '/', [
         "Caller"=>"5557778889"
+    ]);
+    $response
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "text/xml; charset=utf-8")
+        ->assertDontSee(["<?xml version='1.0' encoding='UTF-8'?><Response><Reject/></Response>"], false);
+})->with(['GET', 'POST']);
+
+test('test the blocklist with whitespace in caller number', function ($method) {
+    $caller = "+15557778888";
+    $settingsService = new SettingsService();
+    $settingsService->set("blocklist", $caller);
+    app()->instance(SettingsService::class, $settingsService);
+    $response = $this->call($method, '/', [
+        "Caller" => " 15557778888 "
+    ]);
+    $response
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "text/xml; charset=utf-8")
+        ->assertSeeInOrderExact(["<?xml version='1.0' encoding='UTF-8'?><Response><Reject/></Response>"], false);
+})->with(['GET', 'POST']);
+
+test('test the blocklist with whitespace in blocklist entries', function ($method) {
+    $caller = " +15557778888 , +15557778890 ";
+    $settingsService = new SettingsService();
+    $settingsService->set("blocklist", $caller);
+    app()->instance(SettingsService::class, $settingsService);
+    $response = $this->call($method, '/', [
+        "Caller" => "+15557778890"
+    ]);
+    $response
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "text/xml; charset=utf-8")
+        ->assertSeeInOrderExact(["<?xml version='1.0' encoding='UTF-8'?><Response><Reject/></Response>"], false);
+})->with(['GET', 'POST']);
+
+test('test the blocklist with missing plus sign in caller number', function ($method) {
+    $caller = "+15557778888";
+    $settingsService = new SettingsService();
+    $settingsService->set("blocklist", $caller);
+    app()->instance(SettingsService::class, $settingsService);
+    $response = $this->call($method, '/', [
+        "Caller" => "15557778888"
+    ]);
+    $response
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "text/xml; charset=utf-8")
+        ->assertSeeInOrderExact(["<?xml version='1.0' encoding='UTF-8'?><Response><Reject/></Response>"], false);
+})->with(['GET', 'POST']);
+
+test('test the blocklist with missing plus sign in blocklist entries', function ($method) {
+    $caller = "15557778888,15557778890";
+    $settingsService = new SettingsService();
+    $settingsService->set("blocklist", $caller);
+    app()->instance(SettingsService::class, $settingsService);
+    $response = $this->call($method, '/', [
+        "Caller" => "+15557778890"
+    ]);
+    $response
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "text/xml; charset=utf-8")
+        ->assertSeeInOrderExact(["<?xml version='1.0' encoding='UTF-8'?><Response><Reject/></Response>"], false);
+})->with(['GET', 'POST']);
+
+test('test the blocklist with empty blocklist', function ($method) {
+    $settingsService = new SettingsService();
+    $settingsService->set("blocklist", "");
+    app()->instance(SettingsService::class, $settingsService);
+    $response = $this->call($method, '/', [
+        "Caller" => "+15557778888"
+    ]);
+    $response
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "text/xml; charset=utf-8")
+        ->assertDontSee(["<?xml version='1.0' encoding='UTF-8'?><Response><Reject/></Response>"], false);
+})->with(['GET', 'POST']);
+
+test('test the blocklist with missing Caller parameter', function ($method) {
+    $caller = "+15557778888";
+    $settingsService = new SettingsService();
+    $settingsService->set("blocklist", $caller);
+    app()->instance(SettingsService::class, $settingsService);
+    $response = $this->call($method, '/', []);
+    $response
+        ->assertStatus(200)
+        ->assertHeader("Content-Type", "text/xml; charset=utf-8")
+        ->assertDontSee(["<?xml version='1.0' encoding='UTF-8'?><Response><Reject/></Response>"], false);
+})->with(['GET', 'POST']);
+
+test('test the blocklist with partial match', function ($method) {
+    $caller = "+15557778888";
+    $settingsService = new SettingsService();
+    $settingsService->set("blocklist", $caller);
+    app()->instance(SettingsService::class, $settingsService);
+    $response = $this->call($method, '/', [
+        "Caller" => "+1555777888899"
     ]);
     $response
         ->assertStatus(200)
