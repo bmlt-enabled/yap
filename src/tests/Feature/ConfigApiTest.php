1b27ed5e60409c8a8ae0a1f74060114f3eed554d
Danny Gershman <danny.gershman@gmail.com>
2025-05-21 22:49:13 -0400

    removing old configapi test

--- src/tests/Feature/ConfigApiTest.php
+++ src/tests/Feature/ConfigApiTest.php
@@ -16,28 +16,28 @@ beforeEach(function () {
     $this->rootServerMocks = new RootServerMocks();
 });
 
-test('get config from endpoint', function () {
-    $config = new Settings();
-    $config->title = "welcome to blah";
-    Sanctum::actingAs(User::factory()->create());
-    app()->instance(RootServerService::class, $this->rootServerMocks->getService());
+// test('get config from endpoint', function () {
+//     $config = new Settings();
+//     $config->title = "welcome to blah";
+//     Sanctum::actingAs(User::factory()->create());
+//     app()->instance(RootServerService::class, $this->rootServerMocks->getService());
 
-    ConfigData::createServiceBodyConfiguration(
-        $this->serviceBodyId,
-        $config
-    );
+//     ConfigData::createServiceBodyConfiguration(
+//         $this->serviceBodyId,
+//         $config
+//     );
 
-    $response = $this->call('GET', '/api/v1/config', [
-        "serviceBodyId" => $this->serviceBodyId,
-    ]);
-    $response->assertStatus(200)
-        ->assertHeader("Content-Type", "application/json")
-        ->assertJson([
-            "id"=>1,
-            "service_body_id"=>$this->serviceBodyId,
-            "parent_id"=>0,
-            "data"=>[$config->toArray()]]);
-});
+//     $response = $this->call('GET', '/api/v1/config', [
+//         "serviceBodyId" => $this->serviceBodyId,
+//     ]);
+//     $response->assertStatus(200)
+//         ->assertHeader("Content-Type", "application/json")
+//         ->assertJson([
+//             "id"=>1,
+//             "service_body_id"=>$this->serviceBodyId,
+//             "parent_id"=>0,
+//             "data"=>[$config->toArray()]]);
+// });
 
 //test('get config from endpoint using BMLT no auth', function () {
 //    $this->withoutExceptionHandling();
@@ -57,56 +57,56 @@ test('get config from endpoint', function () {
 //        ->assertHeader("Location", 'http://localhost/api/v1/login');
 //});
 
-test('get config for invalid service body', function () {
-    $config = new Settings();
-    Sanctum::actingAs(User::factory()->create());
-    app()->instance(RootServerService::class, $this->rootServerMocks->getService());
+// test('get config for invalid service body', function () {
+//     $config = new Settings();
+//     Sanctum::actingAs(User::factory()->create());
+//     app()->instance(RootServerService::class, $this->rootServerMocks->getService());
 
-    ConfigData::createServiceBodyConfiguration(
-        99,
-        $config
-    );
+//     ConfigData::createServiceBodyConfiguration(
+//         99,
+//         $config
+//     );
 
-    $this->call('GET', '/api/v1/config', [
-        "serviceBodyId" => 99,
-    ])->assertStatus(200)
-        ->assertHeader("Content-Type", "application/json")
-        ->assertJson([]);
-});
+//     $this->call('GET', '/api/v1/config', [
+//         "serviceBodyId" => 99,
+//     ])->assertStatus(200)
+//         ->assertHeader("Content-Type", "application/json")
+//         ->assertJson([]);
+// });
 
-test('save config', function () {
-    Sanctum::actingAs(User::factory()->create());
-    $serviceBodyConfigData = new Settings();
-    $serviceBodyConfigData->title = "welcome to blah";
-    $response = $this->call('POST', '/api/v1/config', [
-        "serviceBodyId" => $this->serviceBodyId,
-    ], content: json_encode($serviceBodyConfigData));
-    $response->assertStatus(200)
-        ->assertHeader("Content-Type", "application/json")
-        ->assertJson([
-            "id"=>1,
-            "service_body_id"=>$this->serviceBodyId,
-            "parent_id"=>0,
-            "data"=>[$serviceBodyConfigData->toArray()]]);
+// test('save config', function () {
+//     Sanctum::actingAs(User::factory()->create());
+//     $serviceBodyConfigData = new Settings();
+//     $serviceBodyConfigData->title = "welcome to blah";
+//     $response = $this->call('POST', '/api/v1/config', [
+//         "serviceBodyId" => $this->serviceBodyId,
+//     ], content: json_encode($serviceBodyConfigData));
+//     $response->assertStatus(200)
+//         ->assertHeader("Content-Type", "application/json")
+//         ->assertJson([
+//             "id"=>1,
+//             "service_body_id"=>$this->serviceBodyId,
+//             "parent_id"=>0,
+//             "data"=>[$serviceBodyConfigData->toArray()]]);
 
-    $response = $this->call('GET', '/', ['service_body_id'=>$this->serviceBodyId]);
-    $response
-        ->assertStatus(200)
-        ->assertHeader("Content-Type", "text/xml; charset=utf-8")
-        ->assertSeeInOrderExact([
-            '<?xml version="1.0" encoding="UTF-8"?>',
-            '<Response>',
-            '<Gather language="en-US" input="dtmf" numDigits="1" timeout="10" speechTimeout="auto" action="input-method.php" method="GET">',
-            '<Pause length="2"/>',
-            '<Say voice="alice" language="en-US">welcome to blah</Say>',
-            '<Say voice="alice" language="en-US">press one to find someone to talk to</Say>',
-            '<Say voice="alice" language="en-US">press two to search for meetings</Say>',
-            '</Gather>',
-            '</Response>'
-        ], false);
-});
+//     $response = $this->call('GET', '/', ['service_body_id'=>$this->serviceBodyId]);
+//     $response
+//         ->assertStatus(200)
+//         ->assertHeader("Content-Type", "text/xml; charset=utf-8")
+//         ->assertSeeInOrderExact([
+//             '<?xml version="1.0" encoding="UTF-8"?>',
+//             '<Response>',
+//             '<Gather language="en-US" input="dtmf" numDigits="1" timeout="10" speechTimeout="auto" action="input-method.php" method="GET">',
+//             '<Pause length="2"/>',
+//             '<Say voice="alice" language="en-US">welcome to blah</Say>',
+//             '<Say voice="alice" language="en-US">press one to find someone to talk to</Say>',
+//             '<Say voice="alice" language="en-US">press two to search for meetings</Say>',
+//             '</Gather>',
+//             '</Response>'
+//         ], false);
+// });
 
-test('save config with twilio creds', function () {
+<!-- test('save config with twilio creds', function () {
     Sanctum::actingAs(User::factory()->create());
     $serviceBodyConfigData = new Settings();
     $serviceBodyConfigData->twilio_account_sid = "abc";
@@ -125,9 +125,9 @@ test('save config with twilio creds', function () {
     $this->call('GET', '/ping', ['service_body_id'=>$this->serviceBodyId]);
     $this->assertNull(session()->get("override_twilio_account_sid"));
     $this->assertTrue(session()->get("override_mobile_check"));
-});
+}); -->
 
-test('update config', function () {
+<!-- test('update config', function () {
     Sanctum::actingAs(User::factory()->create());
     $serviceBodyConfigData = new Settings();
     $serviceBodyConfigData->title = "welcome to blah";
@@ -152,9 +152,9 @@ test('update config', function () {
             "id"=>1,
             "service_body_id"=>$this->serviceBodyId,
             "parent_id"=>0,
-            "data"=>[$serviceBodyConfigData->toArray()]]);
+            "data"=>[$serviceBodyConfigData->toArray()]]); -->
 
-    $response = $this->call('GET', '/', ['service_body_id'=>$this->serviceBodyId]);
+    <!-- $response = $this->call('GET', '/', ['service_body_id'=>$this->serviceBodyId]);
     $response
         ->assertStatus(200)
         ->assertHeader("Content-Type", "text/xml; charset=utf-8")
@@ -180,4 +180,4 @@ test('get config no auth', function () {
         ->assertHeader("Location", "http://localhost/api/v1/login")
         ->assertHeader("Content-Type", "text/html; charset=utf-8")
         ->assertStatus(302);
-});
+}); -->
