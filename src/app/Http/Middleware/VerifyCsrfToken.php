3dd9c2e2c2727f1aa90ad5f10fb006852362fa05
Danny Gershman <danny.gershman@gmail.com>
2025-12-07 09:42:17 -0500

    adding other missing files

--- src/app/Http/Middleware/VerifyCsrfToken.php
+++ src/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/sanctum/csrf-cookie',
-        'api/*'
+        '*'  // Twilio webhooks don't support CSRF tokens
     ];
 }
